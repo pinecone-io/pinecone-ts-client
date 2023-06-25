ee2e7be1d73187c54d8ee83c0e9b16b84f6ada09
Jen Hamon <jhamon@pinecone.io>
2023-06-25 19:44:18 -0400

    List collections

--- src/control/__tests__/describeCollection.test.ts
+++ src/control/__tests__/describeCollection.test.ts
@@ -75,6 +75,29 @@ describe('describeCollection', () => {
     });
   });
 
+  describe('happy path', () => {
+    test('it should return the collection meta', async () => {
+      const IOA = setupMocks(
+        () =>
+          Promise.resolve({
+            name: 'collection-name',
+            size: 3085509,
+            status: 'Ready',
+          }),
+        () => Promise.resolve([])
+      );
+
+      // @ts-ignore
+      const response = await describeCollection(IOA)('collection-name');
+
+      expect(response).toEqual({
+        name: 'collection-name',
+        size: 3085509,
+        status: 'Ready',
+      });
+    });
+  });
+
   describe('uses http error mapper', () => {
     test('it should map errors with the http error mapper (500)', async () => {
       const IOA = setupMocks(
