153f5012ab6056c8290091a1096d3f24f4ca1add
Austin DeNoble <austin.d@pinecone.io>
2023-08-11 11:18:31 -0400

    remove console.logs

--- src/pinecone-generated-ts-fetch/apis/VectorOperationsApi.ts
+++ src/pinecone-generated-ts-fetch/apis/VectorOperationsApi.ts
@@ -121,7 +121,6 @@ export class VectorOperationsApi extends runtime.BaseAPI {
      */
     async _delete(requestParameters: DeleteOperationRequest, initOverrides?: RequestInit | runtime.InitOverrideFunction): Promise<object> {
         const response = await this._deleteRaw(requestParameters, initOverrides);
-        console.log('response: ', response);
         return await response.value();
     }
 
@@ -166,7 +165,6 @@ export class VectorOperationsApi extends runtime.BaseAPI {
      */
     async delete1(requestParameters: Delete1Request = {}, initOverrides?: RequestInit | runtime.InitOverrideFunction): Promise<object> {
         const response = await this.delete1Raw(requestParameters, initOverrides);
-        console.log('Delete Response: ', response);
         return await response.value();
     }
 
