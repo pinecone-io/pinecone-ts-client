b0eb619dbb9cbfdf352d94ea704f2319f6f8511a
Jennifer Hamon <jhamon@pinecone.io>
2023-10-06 17:17:18 -0400

    Add custom jest test reporters (#134)
    ## Problem
    
    The verbose output from the default reporter is overwhelming now that we
    have 100+ tests. Especially when things fail and you have to scroll back
    through a lot of noise to understand what happened.
     
    ## Solution
    
    Add some reporters that give a smaller amount of output.
    
    ### When passing
    
    <img width="568" alt="Screenshot 2023-10-06 at 4 40 17 PM"
    src="https://github.com/pinecone-io/pinecone-ts-client/assets/1326365/adc60276-0b69-4499-ae12-a372719c0dab">
    
    ### When failing
    
    <img width="912" alt="Screenshot 2023-10-06 at 4 39 57 PM"
    src="https://github.com/pinecone-io/pinecone-ts-client/assets/1326365/24b02ef8-4cc0-407e-adab-95d97f2f31d3">
    
    ### When tests are skipped
    
    <img width="1330" alt="Screenshot 2023-10-06 at 4 45 27 PM"
    src="https://github.com/pinecone-io/pinecone-ts-client/assets/1326365/bfe057e2-c726-487e-94cb-4b98f704805d">
    
    
    ## Type of Change
    
    - [x] Infrastructure change (CI configs, etc)

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,11 @@
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
-  reporters: [['github-actions', { silent: false }], 'default'],
+  reporters: [
+    ['github-actions', { silent: false }],
+    'jest-progress-bar-reporter',
+    'jest-skipped-reporter',
+  ],
   setupFilesAfterEnv: ['./utils/globalUnitTestSetup.ts'],
   transform: {
     '^.+\\.ts?$': 'ts-jest',
