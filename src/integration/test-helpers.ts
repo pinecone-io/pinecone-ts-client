576b3e51104171f7e177a8718680e406a44808ab
Austin DeNoble <austin.d@pinecone.io>
2023-12-07 16:55:15 -0500

    [spruce] Update data plane integration tests (#171)
    ## Problem
    After the updates to the OpenAPI spec and generated core, dataplane
    tests were essentially broken. A lot of `createIndex()` requests
    required updates to the shape of the payload. We were also missing
    coverage for explicit tests involving `fetch` and `update`.
    
    ## Solution
    
    - Update existing `deleteMany.test.ts` and `query.test.ts` suites to
    work properly with the new schema. Mostly involved updating `spec`.
    - Add new `waitUntilReady()` helper to `test-helpers` to allow us to
    poll for freshness after upserting data. This is needed as there's a
    somewhat variable delay in how long it takes for records to be
    queryable.
    - Minor cleanup in `test-helpers`, move the `INDEX_NAME` global variable
    into here, add support for generating metadata.
    - Add new `fetch.test.ts` and `upsertAndUpdate.test.ts` suites. `fetch`
    and `upsert` is somewhat tested in other areas as we use those calls to
    perform other integration tests, but I thought it might be nice to have
    explicit categories for these as well.
    
    ## Type of Change
    - [X] Infrastructure change (CI configs, etc)
    
    ## Test Plan
    Integration tests should be green in CI for both `node` and `edge`:
    <img width="883" alt="Screenshot 2023-12-07 at 10 53 20 AM"
    src="https://github.com/pinecone-io/pinecone-ts-client/assets/119623786/ce73c05d-9394-41eb-9bde-90b0a6fbe74b">
    
    To run locally you can pull this branch down and kick off the
    integration tests:
    `npm run tests:integration:node`
    `npm run tests:integration:edge`

--- src/integration/test-helpers.ts
+++ src/integration/test-helpers.ts
@@ -1,5 +1,18 @@
-import type { PineconeRecord, RecordSparseValues } from '../index';
-import { Pinecone } from '../index';
+import type {
+  IndexStatsDescription,
+  PineconeRecord,
+  RecordSparseValues,
+  RecordMetadata,
+} from '../index';
+import { Pinecone, Index } from '../index';
+
+const metadataMap = {
+  genre: ['action', 'comedy', 'drama', 'horror', 'romance', 'thriller'],
+  year: [2010, 2011, 2012, 2013, 2014, 2015],
+};
+const metadataKeys = Object.keys(metadataMap);
+
+export const INDEX_NAME = 'ts-integration';
 
 export const randomString = (length) => {
   const characters =
@@ -17,13 +30,14 @@ export const randomString = (length) => {
 export const generateRecords = (
   dimension: number,
   quantity: number,
-  withSparseValues?: boolean
+  withSparseValues?: boolean,
+  withMetadata?: boolean
 ): PineconeRecord[] => {
-  const vectors: PineconeRecord[] = [];
+  const records: PineconeRecord[] = [];
   for (let i = 0; i < quantity; i++) {
     const values: number[] = [];
     for (let j = 0; j < dimension; j++) {
-      values.push(Math.random());
+      values.push(parseFloat(Math.random().toFixed(5)));
     }
     let vector: PineconeRecord = {
       id: i.toString(),
@@ -35,10 +49,15 @@ export const generateRecords = (
         sparseValues: generateSparseValues(dimension),
       };
     }
-
-    vectors.push(vector);
+    if (withMetadata) {
+      vector = {
+        ...vector,
+        metadata: generateMetadata(),
+      };
+    }
+    records.push(vector);
   }
-  return vectors;
+  return records;
 };
 
 export const generateSparseValues = (dimension: number): RecordSparseValues => {
@@ -50,6 +69,15 @@ export const generateSparseValues = (dimension: number): RecordSparseValues => {
   }
   const sparseValues: RecordSparseValues = { indices, values };
   return sparseValues;
+};
+
+export const generateMetadata = (): RecordMetadata => {
+  const metaKey = metadataKeys[Math.floor(Math.random() * metadataKeys.length)];
+  const metaValue =
+    metadataMap[metaKey][
+      Math.floor(Math.random() * metadataMap[metaKey].length)
+    ];
+  return { [metaKey]: metaValue };
 };
 
 export const randomIndexName = (testName: string): string => {
@@ -71,4 +99,24 @@ export const waitUntilReady = async (indexName: string) => {
     await sleep(sleepIntervalMs);
     description = await p.describeIndex(indexName);
   }
+};
+
+export const waitUntilRecordsReady = async (
+  index: Index,
+  namespace: string,
+  recordIds: string[]
+): Promise<IndexStatsDescription> => {
+  const sleepIntervalMs = 3000;
+  let indexStats = await index.describeIndexStats();
+
+  while (
+    indexStats.namespaces &&
+    !indexStats.namespaces[namespace] &&
+    indexStats.namespaces[namespace]?.recordCount !== recordIds.length
+  ) {
+    await sleep(sleepIntervalMs);
+    indexStats = await index.describeIndexStats();
+  }
+
+  return indexStats;
 };
