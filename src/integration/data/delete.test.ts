1b08e6824162ab7feffcca98f3a3e3d50cf59c06
Austin DeNoble <austin.d@pinecone.io>
2024-01-12 13:37:24 -0500

    [spruce] Add bun support in CI (#184)
    ## Problem
    Bun has been a rapidly emerging toolkit for developing, testing,
    running, and bundling Javascript and TypeScript. We currently have an
    outstanding GH issue related to the bun runtime:
    https://github.com/pinecone-io/pinecone-ts-client/issues/141. It would
    be nice to add some coverage in CI for running our integration tests
    with bun as the runner.
    
    @jhamon kicked this work off a while ago and I wanted to make sure we
    got this included.
    
    ## Solution
    - Update `/github/workflows/testing.yml` and add a new `config` section
    to the `integration-tests` matrix. Tests will now be run with `npm` in
    node and edge, and `bun` with node for bun versions `1.0.4` -> `1.0.7`.
    
    ## Type of Change
    - [X] Infrastructure change (CI configs, etc)
    
    ## Test Plan
    Make sure all the integration tests are 🟢 in CI.

--- src/integration/data/delete.test.ts
+++ src/integration/data/delete.test.ts
@@ -1,9 +1,9 @@
 import { Pinecone, Index } from '../../index';
 import {
+  assertWithRetries,
   randomString,
   generateRecords,
   INDEX_NAME,
-  sleep,
   waitUntilRecordsReady,
 } from '../test-helpers';
 
@@ -49,7 +49,7 @@ describe('delete', () => {
     await ns.upsert(recordToUpsert);
 
     // Await record freshness, and check record upserted
-    let stats = await waitUntilRecordsReady(ns, namespace, recordIds);
+    const stats = await waitUntilRecordsReady(ns, namespace, recordIds);
     if (stats.namespaces) {
       expect(stats.namespaces[namespace].recordCount).toEqual(1);
     } else {
@@ -57,31 +57,31 @@ describe('delete', () => {
     }
 
     // Look more closely at one of the records to make sure values set
-    const fetchResult = await ns.fetch(['0']);
-    const records = fetchResult.records;
-    if (records) {
-      expect(records['0'].id).toEqual('0');
-      expect(records['0'].values.length).toEqual(5);
-    } else {
-      fail(
-        'Did not find expected records. Fetch result was ' +
-          JSON.stringify(fetchResult)
-      );
-    }
+    const fetchAssertions = [
+      (results) => {
+        if (results.records) {
+          expect(results.records['0'].id).toEqual('0');
+          expect(results.records['0'].values.length).toEqual(5);
+        } else {
+          fail(
+            'Did not find expected records. Fetch result was ' +
+              JSON.stringify(results)
+          );
+        }
+      },
+    ];
+    assertWithRetries(() => ns.fetch(['0']), fetchAssertions);
 
     // Try deleting the record
     await ns.deleteOne('0');
-    await sleep(3000);
 
     // Verify the record is removed
-    stats = await ns.describeIndexStats();
-    if (stats.namespaces) {
-      expect(stats.namespaces[namespace]).toBeUndefined();
-    } else {
-      // no-op. This shouldn't actually happen unless there
-      // are leftover namespaces from previous runs that
-      // failed or stopped without proper cleanup.
-    }
+    const deleteAssertions = [
+      (stats) => {
+        expect(stats.namespaces[namespace]).toBeUndefined();
+      },
+    ];
+    assertWithRetries(() => ns.describeIndexStats(), deleteAssertions);
   });
 
   test('verify deleteMany with ids', async () => {
@@ -95,7 +95,7 @@ describe('delete', () => {
     await ns.upsert(recordsToUpsert);
 
     // Await record freshness, and check records upserted
-    let stats = await waitUntilRecordsReady(ns, namespace, recordIds);
+    const stats = await waitUntilRecordsReady(ns, namespace, recordIds);
     if (stats.namespaces) {
       expect(stats.namespaces[namespace].recordCount).toEqual(3);
     } else {
@@ -103,55 +103,68 @@ describe('delete', () => {
     }
 
     // Look more closely at one of the records to make sure values set
-    const fetchResult = await ns.fetch(['0']);
-    const records = fetchResult.records;
-    if (records) {
-      expect(records['0'].id).toEqual('0');
-      expect(records['0'].values.length).toEqual(5);
-    } else {
-      fail(
-        'Did not find expected records. Fetch result was ' +
-          JSON.stringify(fetchResult)
-      );
-    }
+    const fetchAssertions = [
+      (results) => {
+        if (results.records) {
+          expect(results.records['0'].id).toEqual('0');
+          expect(results.records['0'].values.length).toEqual(5);
+        } else {
+          fail(
+            'Did not find expected records. Fetch result was ' +
+              JSON.stringify(results)
+          );
+        }
+      },
+    ];
+    assertWithRetries(() => ns.fetch(['0']), fetchAssertions);
 
     // Try deleting 2 of 3 records
     await ns.deleteMany(['0', '2']);
-    await sleep(3000);
-    stats = await ns.describeIndexStats();
-    if (stats.namespaces) {
-      expect(stats.namespaces[namespace].recordCount).toEqual(1);
-    } else {
-      fail(
-        'Expected namespaces to be defined (second call). Stats were ' +
-          JSON.stringify(stats)
-      );
-    }
+
+    const deleteAssertions = [
+      (stats) => {
+        if (stats.namespaces) {
+          expect(stats.namespaces[namespace].recordCount).toEqual(1);
+        } else {
+          fail(
+            'Expected namespaces to be defined (second call). Stats were ' +
+              JSON.stringify(stats)
+          );
+        }
+      },
+    ];
+    assertWithRetries(() => ns.describeIndexStats(), deleteAssertions);
 
     // Check that record id='1' still exists
-    const fetchResult2 = await ns.fetch(['1']);
-    const records2 = fetchResult2.records;
-    if (records2) {
-      expect(records2['1']).not.toBeUndefined();
-    } else {
-      fail(
-        'Expected record 2 to be defined. Fetch result was ' +
-          JSON.stringify(fetchResult2)
-      );
-    }
+    const fetchAssertions2 = [
+      (results) => {
+        if (results.records2) {
+          expect(results.records2['1']).not.toBeUndefined();
+        } else {
+          fail(
+            'Expected record 2 to be defined. Fetch result was ' +
+              JSON.stringify(results)
+          );
+        }
+      },
+    ];
+    assertWithRetries(() => ns.fetch(['1']), fetchAssertions2);
 
     // deleting non-existent records should not throw
     await ns.deleteMany(['0', '1', '2', '3']);
-    await sleep(3000);
 
     // Verify all are now removed
-    stats = await ns.describeIndexStats();
-    if (stats.namespaces) {
-      expect(stats.namespaces[namespace]).toBeUndefined();
-    } else {
-      // no-op. This shouldn't actually happen unless there
-      // are leftover namespaces from previous runs that
-      // failed or stopped without proper cleanup.
-    }
+    const deleteAssertions2 = [
+      (stats) => {
+        if (stats.namespaces) {
+          expect(stats.namespaces[namespace]).toBeUndefined();
+        } else {
+          // no-op. This shouldn't actually happen unless there
+          // are leftover namespaces from previous runs that
+          // failed or stopped without proper cleanup.
+        }
+      },
+    ];
+    assertWithRetries(() => ns.describeIndexStats(), deleteAssertions2);
   });
 });
