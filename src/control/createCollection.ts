a5645068ecdf85b4787a5bd27713a36d3cbb3e41
aulorbe <aulorbe@gmail.com>
2024-08-08 15:06:50 -0700

    createCollection

--- src/control/createCollection.ts
+++ src/control/createCollection.ts
@@ -14,20 +14,37 @@ import {PineconeArgumentError} from "../errors";
 
 export const createCollection = (api: ManageIndexesApi) => {
     const createCollectionsValidator = (options: CreateCollectionRequest) => {
-        if (!options.name || options.name.length === 0) {
+        // Check if options is provided, if it's an object, and if it's not an array
+        if (!options || typeof options !== 'object' || Array.isArray(options) || Object.keys(options).length === 0) {
             throw new PineconeArgumentError(
-                'You must enter a non-empty string for `name` field.'
+                'The argument passed to createCollection must be a non-empty object.'
             );
         }
-        if (!options.source || options.source.length === 0) {
+
+        // Confirm req'd fields are present
+        const requiredFields = ['name', 'source'];
+        for (const field of requiredFields) {
+            if (!options[field] || options[field].length === 0 || typeof options[field] !== 'string') {
+                throw new PineconeArgumentError(
+                    `You must enter a non-empty string for the \`${field}\` field.`
+                );
+            }
+        }
+
+
+        // Confirm there aren't extraneous fields
+        const optionKeys = Object.keys(options);
+        const extraneousFields = optionKeys.filter(key => !requiredFields.includes(key));
+        if (extraneousFields.length > 0) {
             throw new PineconeArgumentError(
-                'You must enter a non-empty string for `source` field.'
+                `Extraneous fields are not allowed: ${extraneousFields.join(', ')}`
             );
         }
     };
 
     return async (options: CreateCollectionRequest): Promise<CollectionModel> => {
         createCollectionsValidator(options);
+
         return await api.createCollection({createCollectionRequest: options});
     };
 };
