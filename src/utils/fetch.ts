8324f122e44d03b8e2398782ae88f258781f4b71
Audrey Sage Lorberfeld <aulorbe@gmail.com>
2024-09-12 12:49:21 -0700

    Add example of proxying (#281)
    ## Problem
    
    The community has requested clearer direction regarding using proxy
    servers and other http-related customizations with our Typescript
    client.
    
    Asana ticket https://app.asana.com/0/1203260648987893/1205491455268495/f
    
    ## Solution
    
    Add proxying section to README + clarify some code comments.
    
    ## Type of Change
    
    - [ ] Bug fix (non-breaking change which fixes an issue)
    - [ ] New feature (non-breaking change which adds functionality)
    - [ ] Breaking change (fix or feature that would cause existing
    functionality to not work as expected)
    - [ ] This change requires a documentation update
    - [ ] Infrastructure change (CI configs, etc)
    - [x] Non-code change (docs, etc)
    - [ ] None of the above: (explain here)
    
    ## Test Plan
    
    Describe specific steps for validating this change.
    
    
    ---
    - To see the specific tasks where the Asana app for GitHub is being
    used, see below:
      - https://app.asana.com/0/0/1205491455268495

--- src/utils/fetch.ts
+++ src/utils/fetch.ts
@@ -6,12 +6,13 @@ export const getFetch = (config: PineconeConfiguration) => {
     // User-provided fetch implementation, if any, takes precedence.
     return config.fetchApi;
   } else if (global.fetch) {
-    // If a fetch implementation is already present in the global
-    // scope, use that. This should prevent confusing failures in
-    // nextjs projects where @vercel/fetch is mandated and
+    // If a fetch implementation is present in the global scope (will work with native fetch in Node18+, Edge runtimes,
+    // etc.), use that. This should prevent confusing failures in
+    // Next.js projects where @vercel/fetch is mandated and
     // other implementations are stubbed out.
     return global.fetch;
   } else {
+    // If no fetch implementation is found, throw an error.
     throw new PineconeConfigurationError(
       'No global or user-provided fetch implementations found. Please supply a fetch implementation.'
     );
