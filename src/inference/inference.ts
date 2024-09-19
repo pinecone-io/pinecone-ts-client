877588fcdee51efa02a909e0a787aaa624830f85
aulorbe <aulorbe@gmail.com>
2024-09-19 13:47:11 -0700

    Finalize rerank; just need to write tests

--- src/inference/inference.ts
+++ src/inference/inference.ts
@@ -3,12 +3,13 @@ import {
   EmbedRequestInputsInner,
   EmbedRequestParameters,
   InferenceApi,
-  Rerank200Response as RerankResult,
+  Rerank200Response as RerankResponse,
 } from '../pinecone-generated-ts-fetch/inference';
 import { EmbeddingsList } from '../models';
 import { PineconeArgumentError } from '../errors';
+import { prerelease } from '../utils/prerelease';
 
-interface RerankOptions {
+export interface RerankOptions {
   topN?: number;
   returnDocuments?: boolean;
   rankFields?: Array<string>;
@@ -57,14 +58,80 @@ export class Inference {
     return new EmbeddingsList(response.model, response.data, response.usage);
   }
 
-  // todo: docstring
+  /** Rerank documents against a query with a reranking model. Each document is ranked in descending relevance order
+   *  against the query provided.
+   *
+   *  Note: by default, the ['text'] field of each document is used for ranking; you can overwrite this default
+   *  behavior by passing an {@link RerankOptions} `options` object specifying 1+ other fields.
+   *
+   *  @example
+   *  ```typescript
+   *  import { Pinecone } from '@pinecone-database/pinecone';
+   *  const pc = new Pinecone();
+   *  const rerankingModel = "bge-reranker-v2-m3";
+   *  const myQuery = "What are some good Turkey dishes for Thanksgiving?";
+   *  const myDocuments = [
+   *  { text: "I love turkey sandwiches with pastrami" },
+   *  { text: "A lemon brined Turkey with apple sausage stuffing is a classic Thanksgiving main" },
+   *  { text: "My favorite Thanksgiving dish is pumpkin pie" },
+   *  { text: "Turkey is a great source of protein" },
+   *  ];
+   *
+   *  // >>> Sample without passing an `options` object:
+   *  const response = await pc.inference.rerank(rerankingModel, myQuery, myDocuments);
+   *  console.log(response);
+   *  // {
+   *  // model: 'bge-reranker-v2-m3',
+   *  // data: [
+   *  //   { index: 1, score: 0.5633179, document: [Object] },
+   *  //   { index: 2, score: 0.02013874, document: [Object] },
+   *  //   { index: 3, score: 0.00035419367, document: [Object] },
+   *  //   { index: 0, score: 0.00021485926, document: [Object] }
+   *  // ],
+   *  // usage: { rerankUnits: 1 }
+   *  // }
+   *
+   *
+   *  // >>> Sample with an `options` object:
+   *  const rerankOptions = {
+   *     topN: 3,
+   *     returnDocuments: false
+   * }
+   *  const response = await pc.inference.rerank(rerankingModel, myQuery, myDocuments, rerankOptions);
+   *  console.log(response);
+   *  // {
+   *  // model: 'bge-reranker-v2-m3',
+   *  // data: [
+   *  //   { index: 1, score: 0.5633179, document: undefined },
+   *  //   { index: 2, score: 0.02013874, document: undefined },
+   *  //   { index: 3, score: 0.00035419367, document: undefined },
+   *  // ],
+   *  // usage: { rerankUnits: 1 }
+   *  //}
+   *  ```
+   *
+   * @param model - (Required) The model to use for reranking. Currently, the only available model is "[bge-reranker-v2-m3](https://docs.pinecone.io/models/bge-reranker-v2-m3)"}.
+   * @param query - (Required) The query to rerank documents against.
+   * @param documents - (Required) The documents to rerank. Each document must be either a string or a dictionary
+   * with (at minimum) a `text` key.
+   * @param options - (Optional) Additional options to send with the core reranking request. Options include: how many
+   * results to return, whether to return the documents in the response, alternative fields by which the model
+   * should to rank the documents, and additional model-specific parameters. See {@link RerankOptions} for more details.
+   * */
+  @prerelease('2024-10')
   async rerank(
     model: string,
     query: string,
     documents: Array<{ [key: string]: string } | string>,
     options: RerankOptions = {}
-  ): Promise<RerankResult> {
+  ): Promise<RerankResponse> {
+    if (documents.length == 0) {
+      throw new PineconeArgumentError(
+        'You must pass at least one document to rerank'
+      );
+    }
     // Destructure `options` with defaults
+    // Note: If the user passes in key:value pairs in `options` that are not the following, they are ignored
     const {
       topN = documents.length,
       returnDocuments = true,
@@ -111,8 +178,6 @@ export class Inference {
       },
     };
 
-    return (await this._inferenceApi.rerank(req)) as RerankResult;
+    return await this._inferenceApi.rerank(req);
   }
 }
-
-// todo: add feature flag from bulk import PR here when that PR's merged
