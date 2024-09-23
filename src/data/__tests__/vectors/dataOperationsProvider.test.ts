2cb2517319f243f6860991fbf616f9128563f20c
aulorbe <aulorbe@gmail.com>
2024-09-23 13:22:29 -0700

    Fix relative path according to new reog

--- src/data/__tests__/vectors/dataOperationsProvider.test.ts
+++ src/data/__tests__/vectors/dataOperationsProvider.test.ts
@@ -3,7 +3,7 @@ import { IndexHostSingleton } from '../../indexHostSingleton';
 import { Configuration } from '../../../pinecone-generated-ts-fetch/db_data';
 
 jest.mock('../../../pinecone-generated-ts-fetch/db_data', () => ({
-  ...jest.requireActual('../../pinecone-generated-ts-fetch/db_data'),
+  ...jest.requireActual('../../../pinecone-generated-ts-fetch/db_data'),
   Configuration: jest.fn(),
 }));
 
