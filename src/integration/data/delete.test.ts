c28f6ffb3197b69ab96c3c2939dcc2baf7f50447
aulorbe <aulorbe@gmail.com>
2024-10-01 18:07:53 -0700

    Save work on delete tests

--- src/integration/data/delete.test.ts
+++ src/integration/data/delete.test.ts
@@ -1,164 +1,94 @@
 import { Pinecone, Index } from '../../index';
 import {
-  assertWithRetries,
-  randomString,
   generateRecords,
-  INDEX_NAME,
   waitUntilRecordsReady,
+  globalNamespaceOne,
+  sleep,
 } from '../test-helpers';
 
-describe('delete', () => {
-  let pinecone: Pinecone,
-    index: Index,
-    ns: Index,
-    namespace: string,
-    recordIds: string[];
+let pinecone: Pinecone,
+  serverlessIndexName: string,
+  serverlessIndex: Index,
+  recordIds: string[];
 
-  beforeEach(async () => {
-    pinecone = new Pinecone();
+beforeAll(async () => {
+  pinecone = new Pinecone();
+  serverlessIndexName = 'integration-test-serverless-delete';
 
-    await pinecone.createIndex({
-      name: INDEX_NAME,
-      dimension: 5,
-      metric: 'cosine',
-      spec: {
-        serverless: {
-          region: 'us-west-2',
-          cloud: 'aws',
-        },
+  await pinecone.createIndex({
+    name: serverlessIndexName,
+    dimension: 5,
+    metric: 'cosine',
+    spec: {
+      serverless: {
+        region: 'us-west-2',
+        cloud: 'aws',
       },
-      waitUntilReady: true,
-      suppressConflicts: true,
-    });
-
-    namespace = randomString(16);
-    index = pinecone.index(INDEX_NAME);
-    ns = index.namespace(namespace);
+    },
+    waitUntilReady: true,
+    suppressConflicts: true,
   });
 
-  afterAll(async () => {
-    await ns.deleteMany(recordIds);
-  });
+  serverlessIndex = pinecone
+    .index(serverlessIndexName)
+    .namespace(globalNamespaceOne);
 
-  test('verify delete with id', async () => {
-    const recordToUpsert = generateRecords({ dimension: 5, quantity: 1 });
-    recordIds = recordToUpsert.map((r) => r.id);
-    expect(recordToUpsert).toHaveLength(1);
-    expect(recordToUpsert[0].id).toEqual(recordIds[0]);
+  // Seed index
+  const recordsToUpsert = generateRecords({ dimension: 5, quantity: 5 });
+  recordIds = recordsToUpsert.map((r) => r.id);
+  await serverlessIndex.upsert(recordsToUpsert);
+});
 
-    await ns.upsert(recordToUpsert);
+afterAll(async () => {
+  await pinecone.deleteIndex(serverlessIndexName);
+});
 
+// todo: deleting non-existent records
+// todo: delete all records in namespace
+// todo: delete namespace
+// todo: delete index (?)
+
+describe('delete', () => {
+  test('verify delete with an id', async () => {
     // Await record freshness, and check record upserted
-    const stats = await waitUntilRecordsReady(ns, namespace, recordIds);
+    const stats = await waitUntilRecordsReady(
+      serverlessIndex,
+      globalNamespaceOne,
+      recordIds
+    );
     if (stats.namespaces) {
-      expect(stats.namespaces[namespace].recordCount).toEqual(1);
+      expect(stats.namespaces[globalNamespaceOne].recordCount).toEqual(5);
     } else {
       fail('Expected namespaces to be defined');
     }
 
-    // Look more closely at one of the records to make sure values set
-    const fetchAssertions = (results) => {
-      if (results.records) {
-        expect(results.records['0'].id).toEqual('0');
-        expect(results.records['0'].values.length).toEqual(5);
-      } else {
-        fail(
-          'Did not find expected records. Fetch result was ' +
-            JSON.stringify(results)
-        );
-      }
-    };
-
-    await assertWithRetries(() => ns.fetch(['0']), fetchAssertions);
-
     // Try deleting the record
-    await ns.deleteOne('0');
+    await serverlessIndex.deleteOne('0');
 
     // Verify the record is removed
     const deleteAssertions = (stats) => {
-      expect(stats.namespaces[namespace]).toBeUndefined();
+      expect(stats.namespaces[globalNamespaceOne].recordCount).toEqual(4);
     };
 
-    await assertWithRetries(() => ns.describeIndexStats(), deleteAssertions);
+    deleteAssertions(await serverlessIndex.describeIndexStats());
   });
 
-  test('verify deleteMany with ids', async () => {
-    const recordsToUpsert = generateRecords({ dimension: 5, quantity: 3 });
-    recordIds = recordsToUpsert.map((r) => r.id);
-    expect(recordsToUpsert).toHaveLength(3);
-    expect(recordsToUpsert[0].id).toEqual('0');
-    expect(recordsToUpsert[1].id).toEqual('1');
-    expect(recordsToUpsert[2].id).toEqual('2');
+  test('verify deleteMany with multiple ids', async () => {
+    // Try deleting 2 of 5 records (note: ID '0' was deleted in previous test)
+    await serverlessIndex.deleteMany(['1', '2']);
 
-    await ns.upsert(recordsToUpsert);
-
-    // Await record freshness, and check records upserted
-    const stats = await waitUntilRecordsReady(ns, namespace, recordIds);
-    if (stats.namespaces) {
-      expect(stats.namespaces[namespace].recordCount).toEqual(3);
-    } else {
-      fail('Expected namespaces to be defined');
-    }
-
-    // Look more closely at one of the records to make sure values set
-    const fetchAssertions = (results) => {
-      if (results.records) {
-        expect(results.records['0'].id).toEqual('0');
-        expect(results.records['0'].values.length).toEqual(5);
-      } else {
-        fail(
-          'Did not find expected records. Fetch result was ' +
-            JSON.stringify(results)
-        );
-      }
-    };
-
-    await assertWithRetries(() => ns.fetch(['0']), fetchAssertions);
-
-    // Try deleting 2 of 3 records
-    await ns.deleteMany(['0', '2']);
+    await sleep(19000); // this is way too long to wait; so flakey
 
     const deleteAssertions = (stats) => {
       if (stats.namespaces) {
-        expect(stats.namespaces[namespace].recordCount).toEqual(1);
+        expect(stats.namespaces[globalNamespaceOne].recordCount).toEqual(2);
       } else {
         fail(
-          'Expected namespaces to be defined (second call). Stats were ' +
-            JSON.stringify(stats)
+          `Expected 2 records in the namespace: ${globalNamespaceOne}, but found ${stats.namespaces[globalNamespaceOne].recordCount}`
         );
       }
     };
 
-    await assertWithRetries(() => ns.describeIndexStats(), deleteAssertions);
-
-    // Check that record id='1' still exists
-    const fetchAssertions2 = (results) => {
-      if (results.records2) {
-        expect(results.records2['1']).not.toBeUndefined();
-      } else {
-        fail(
-          'Expected record 2 to be defined. Fetch result was ' +
-            JSON.stringify(results)
-        );
-      }
-    };
-
-    await assertWithRetries(() => ns.fetch(['1']), fetchAssertions2);
-
-    // deleting non-existent records should not throw
-    await ns.deleteMany(['0', '1', '2', '3']);
-
-    // Verify all are now removed
-    const deleteAssertions2 = (stats) => {
-      if (stats.namespaces) {
-        expect(stats.namespaces[namespace]).toBeUndefined();
-      } else {
-        // no-op. This shouldn't actually happen unless there
-        // are leftover namespaces from previous runs that
-        // failed or stopped without proper cleanup.
-      }
-    };
-
-    await assertWithRetries(() => ns.describeIndexStats(), deleteAssertions2);
+    deleteAssertions(await serverlessIndex.describeIndexStats());
   });
 });
