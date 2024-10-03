1375ce60dabb1a0c4d0f1c8565f93b30ce9046c0
aulorbe <aulorbe@gmail.com>
2024-10-03 15:17:11 -0700

    Try allow configureIndex to pass by removing index creation/deletion from error cases block

--- src/integration/control/configureIndex.test.ts
+++ src/integration/control/configureIndex.test.ts
@@ -4,51 +4,51 @@ import { randomIndexName, waitUntilReady } from '../test-helpers';
 
 let podIndexName: string, serverlessIndexName: string, pinecone: Pinecone;
 
-beforeAll(async () => {
-  pinecone = new Pinecone();
-  podIndexName = randomIndexName('pod-configure');
-  serverlessIndexName = randomIndexName('-serverless-configure');
-
-  // create pod index
-  await pinecone.createIndex({
-    name: podIndexName,
-    dimension: 5,
-    metric: 'cosine',
-    spec: {
-      pod: {
-        environment: 'us-east1-gcp',
-        podType: 'p1.x1',
-        pods: 1,
-      },
-    },
-    waitUntilReady: true,
-  });
-
-  // create serverless index
-  await pinecone.createIndex({
-    name: serverlessIndexName,
-    dimension: 5,
-    metric: 'cosine',
-    spec: {
-      serverless: {
-        cloud: 'aws',
-        region: 'us-east-1',
-      },
-    },
-    waitUntilReady: true,
-  });
-});
-
-afterAll(async () => {
-  // wait until indexes are done upgrading before deleting
-  await waitUntilReady(podIndexName);
-  await waitUntilReady(serverlessIndexName);
-
-  await pinecone.deleteIndex(podIndexName);
-  await pinecone.deleteIndex(serverlessIndexName);
-});
-
 describe('configure index', () => {
+  beforeAll(async () => {
+    pinecone = new Pinecone();
+    podIndexName = randomIndexName('pod-configure');
+    serverlessIndexName = randomIndexName('-serverless-configure');
+
+    // create pod index
+    await pinecone.createIndex({
+      name: podIndexName,
+      dimension: 5,
+      metric: 'cosine',
+      spec: {
+        pod: {
+          environment: 'us-east1-gcp',
+          podType: 'p1.x1',
+          pods: 1,
+        },
+      },
+      waitUntilReady: true,
+    });
+
+    // create serverless index
+    await pinecone.createIndex({
+      name: serverlessIndexName,
+      dimension: 5,
+      metric: 'cosine',
+      spec: {
+        serverless: {
+          cloud: 'aws',
+          region: 'us-east-1',
+        },
+      },
+      waitUntilReady: true,
+    });
+  });
+
+  afterAll(async () => {
+    // wait until indexes are done upgrading before deleting
+    await waitUntilReady(podIndexName);
+    await waitUntilReady(serverlessIndexName);
+
+    await pinecone.deleteIndex(podIndexName);
+    await pinecone.deleteIndex(serverlessIndexName);
+  });
+
   describe('pod index', () => {
     test('scale replicas up', async () => {
       const description = await pinecone.describeIndex(podIndexName);
@@ -100,76 +100,76 @@ describe('configure index', () => {
       });
     });
   });
+});
 
-  describe('error cases', () => {
-    test('cannot configure index with invalid index name', async () => {
-      try {
-        await pinecone.configureIndex('non-existent-index', {
-          spec: { pod: { replicas: 2 } },
-        });
-      } catch (e) {
-        const err = e as BasePineconeError;
-        expect(err.name).toEqual('PineconeNotFoundError');
-      }
-    });
+describe('error cases', () => {
+  test('cannot configure index with invalid index name', async () => {
+    try {
+      await pinecone.configureIndex('non-existent-index', {
+        spec: { pod: { replicas: 2 } },
+      });
+    } catch (e) {
+      const err = e as BasePineconeError;
+      expect(err.name).toEqual('PineconeNotFoundError');
+    }
+  });
 
-    test('cannot configure index when exceeding quota', async () => {
-      try {
-        await pinecone.configureIndex(podIndexName, {
-          spec: { pod: { replicas: 20 } },
-        });
-      } catch (e) {
-        const err = e as BasePineconeError;
-        expect(err.name).toEqual('PineconeBadRequestError');
-        expect(err.message).toContain(
-          `You've reached the max pods allowed in project`
-        );
-        expect(err.message).toContain(
-          'To increase this limit, adjust your project settings in the console'
-        );
-      }
-    });
+  test('cannot configure index when exceeding quota', async () => {
+    try {
+      await pinecone.configureIndex(podIndexName, {
+        spec: { pod: { replicas: 20 } },
+      });
+    } catch (e) {
+      const err = e as BasePineconeError;
+      expect(err.name).toEqual('PineconeBadRequestError');
+      expect(err.message).toContain(
+        `You've reached the max pods allowed in project`
+      );
+      expect(err.message).toContain(
+        'To increase this limit, adjust your project settings in the console'
+      );
+    }
+  });
 
-    test('cannot change base pod type', async () => {
-      try {
-        // Try to change the base pod type
-        await pinecone.configureIndex(podIndexName, {
-          spec: { pod: { podType: 'p2.x1' } },
-        });
-      } catch (e) {
-        const err = e as BasePineconeError;
-        expect(err.name).toEqual('PineconeBadRequestError');
-        expect(err.message).toContain('Bad request: Cannot change pod type');
-      }
-    });
+  test('cannot change base pod type', async () => {
+    try {
+      // Try to change the base pod type
+      await pinecone.configureIndex(podIndexName, {
+        spec: { pod: { podType: 'p2.x1' } },
+      });
+    } catch (e) {
+      const err = e as BasePineconeError;
+      expect(err.name).toEqual('PineconeBadRequestError');
+      expect(err.message).toContain('Bad request: Cannot change pod type');
+    }
+  });
 
-    test('cannot set deletionProtection value other than enabled / disabled', async () => {
-      try {
-        await pinecone.configureIndex(serverlessIndexName, {
-          // @ts-expect-error
-          deletionProtection: 'bogus',
-        });
-      } catch (e) {
-        const err = e as BasePineconeError;
-        expect(err.name).toEqual('PineconeBadRequestError');
-        expect(err.message).toContain(
-          'Invalid deletion_protection, value should be either enabled or disabled'
-        );
-      }
-    });
+  test('cannot set deletionProtection value other than enabled / disabled', async () => {
+    try {
+      await pinecone.configureIndex(serverlessIndexName, {
+        // @ts-expect-error
+        deletionProtection: 'bogus',
+      });
+    } catch (e) {
+      const err = e as BasePineconeError;
+      expect(err.name).toEqual('PineconeBadRequestError');
+      expect(err.message).toContain(
+        'Invalid deletion_protection, value should be either enabled or disabled'
+      );
+    }
+  });
 
-    test('cannot configure pod spec for serverless', async () => {
-      try {
-        await pinecone.configureIndex(serverlessIndexName, {
-          spec: { pod: { replicas: 2 } },
-        });
-      } catch (e) {
-        const err = e as BasePineconeError;
-        expect(err.name).toEqual('PineconeBadRequestError');
-        expect(err.message).toContain(
-          'Configuring replicas and pod type is not supported for serverless'
-        );
-      }
-    });
+  test('cannot configure pod spec for serverless', async () => {
+    try {
+      await pinecone.configureIndex(serverlessIndexName, {
+        spec: { pod: { replicas: 2 } },
+      });
+    } catch (e) {
+      const err = e as BasePineconeError;
+      expect(err.name).toEqual('PineconeBadRequestError');
+      expect(err.message).toContain(
+        'Configuring replicas and pod type is not supported for serverless'
+      );
+    }
   });
 });
