dbda5b496bade694ffb43a038f97c85aa10ca033
aulorbe <aulorbe@gmail.com>
2024-10-03 13:43:49 -0700

    Remove global setup, teardown from node jest config

--- jest.config.integration-node.js
+++ jest.config.integration-node.js
@@ -3,8 +3,6 @@ const config = require('./jest.config');
 module.exports = {
   ...config,
   reporters: [['github-actions', { silent: false }], 'default'],
-  globalSetup: './src/integration/callSetup.js',
-  globalTeardown: './src/integration/callTeardown.js',
   transform: {
     '^.+\\.ts?$': 'ts-jest',
   },
