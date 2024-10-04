9ac32bdde804030105d336225e1a420989eae0ac
aulorbe <aulorbe@gmail.com>
2024-10-04 11:11:05 -0700

    No longer need transform here b/c not using Jest globals

--- jest.config.integration-node.js
+++ jest.config.integration-node.js
@@ -3,9 +3,6 @@ const config = require('./jest.config');
 module.exports = {
   ...config,
   reporters: [['github-actions', { silent: false }], 'default'],
-  transform: {
-    '^.+\\.ts?$': 'ts-jest',
-  },
   setupFilesAfterEnv: ['./utils/globalIntegrationTestSetup.ts'],
   testPathIgnorePatterns: [],
   testEnvironment: 'node',
