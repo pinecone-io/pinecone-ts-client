5192e4c4fbf67f493a44616909e6f94ec59bda97
aulorbe <aulorbe@gmail.com>
2024-10-04 09:47:24 -0700

    Try full run!

--- src/integration/teardown.ts
+++ src/integration/teardown.ts
@@ -1,10 +1,5 @@
 import { Pinecone } from '../pinecone';
 import { serverlessIndexName } from './test-helpers';
-//
-// module.exports = async function () {
-//   await teardown();
-//   return null;
-// };
 
 export const teardown = async () => {
   const pc = new Pinecone({ apiKey: process.env['PINECONE_API_KEY']! });
