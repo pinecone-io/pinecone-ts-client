2b67942c58591532cc65d8d6df05c0f2c51726a2
aulorbe <aulorbe@gmail.com>
2024-10-28 10:48:08 -0700

    Encapsulate test logic into ts script instead of in GH action file

---
+++ src/end-to-end/localRuns.ts
@@ -0,0 +1,45 @@
+class EndToEndEdgeTest {
+  constructor(public readonly apiKey: string) {
+    this.apiKey = apiKey;
+  }
+  url = 'https://ts-client-e2e-tests.vercel.app/api/createSeedQuery';
+
+  hitEndpoint = async (url: string) => {
+    const response = await fetch(url, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        pinecone_api_key: this.apiKey,
+      },
+    });
+
+    if (!response || response.status !== 200) {
+      throw new Error(`Failed to hit endpoint: ${response.status}`);
+    }
+
+    return response.json();
+  };
+
+  assertOnResponse = async () => {
+    const queryResponse = await this.hitEndpoint(this.url);
+    if (!(queryResponse['queryResult']['matches'].length >= 1)) {
+      throw new Error(
+        `Test failure, query response is empty: ${queryResponse}`
+      );
+    } else {
+      return queryResponse['indexName'];
+    }
+  };
+}
+
+const apiKey = process.argv[2];
+
+if (!apiKey) {
+  throw new Error('PINECONE_API_KEY key is required');
+}
+
+const edgeTest = new EndToEndEdgeTest(apiKey);
+
+edgeTest.assertOnResponse().catch((error) => {
+  console.error('Test failed:', error);
+});
