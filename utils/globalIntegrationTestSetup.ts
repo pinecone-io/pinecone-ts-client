ee798d7db0f937c27755d0e1d659fa55603c0133
aulorbe <aulorbe@gmail.com>
2024-10-31 16:53:20 -0700

    Oops, remove holdover from when we were spinning up an Express server

--- utils/globalIntegrationTestSetup.ts
+++ utils/globalIntegrationTestSetup.ts
@@ -2,8 +2,6 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
-process.env.NODE_OPTIONS = '--no-restricted-code-eval';
-
 const requiredEnvVars = ['PINECONE_API_KEY'];
 for (const envVar of requiredEnvVars) {
   if (!process.env[envVar]) {
