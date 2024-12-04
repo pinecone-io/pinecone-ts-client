45c026b3f4ae9cfe90c500a56bd2eee0e7ccb9fa
aulorbe <aulorbe@gmail.com>
2024-12-03 16:33:32 -0800

    Add latest code gen for 2025-01 RC

---
+++ src/pinecone-generated-ts-fetch/assistant_control/models/index.ts
@@ -0,0 +1,9 @@
+/* tslint:disable */
+/* eslint-disable */
+export * from './Assistant';
+export * from './CreateAssistantRequest';
+export * from './ListAssistants200Response';
+export * from './ListAssistants401Response';
+export * from './ListAssistants401ResponseError';
+export * from './UpdateAssistant200Response';
+export * from './UpdateAssistantRequest';
