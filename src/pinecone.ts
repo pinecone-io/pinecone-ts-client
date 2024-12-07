22070f7866c9ece137679b620f6a948f666c2b12
aulorbe <aulorbe@gmail.com>
2024-12-06 16:32:54 -0800

    Save work

--- src/pinecone.ts
+++ src/pinecone.ts
@@ -680,7 +680,7 @@ export class Pinecone {
 
   Assistant(assistantName: string) {
     const assistantApiData = assistantDataOperationsBuilder(this.config);
-    return new AssistantDataPlane(assistantApiData);
+    return new AssistantDataPlane(assistantApiData, assistantName);
   }
 
   // Uppercase AssistantCtrlPlane == dataplane
