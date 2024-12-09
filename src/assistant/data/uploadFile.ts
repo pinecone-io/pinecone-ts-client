37952adcf9de9b972f3ea0de6729570bae023546
aulorbe <aulorbe@gmail.com>
2024-12-09 11:02:34 -0800

    Got chat working!

--- src/assistant/data/uploadFile.ts
+++ src/assistant/data/uploadFile.ts
@@ -12,20 +12,20 @@ export interface UploadRequest {
 }
 
 
-async function filePathToBlob(filePath: string): Promise<Blob> {
-  // Read the file as a Buffer
-  const fileBuffer = await fs.readFile(filePath);
-  // Create a Blob from the Buffer
-}
+// async function filePathToBlob(filePath: string): Promise<Blob> {
+//   // Read the file as a Buffer
+//   const fileBuffer = await fs.readFile(filePath);
+//   // Create a Blob from the Buffer
+// }
 
 export const uploadFileClosed = (api: ManageAssistantsApiData) => {
-  return async (options: UploadRequest): Promise<AssistantFileModel> => {
-    const path = await filePathToBlob(options.filePath);
+  // return async (options: UploadRequest): Promise<AssistantFileModel> => {
+    // const path = await filePathToBlob(options.filePath);
 
-    console.log("Type of path = ", typeof path);
-
-    const request: UploadFileRequest = {assistantName: options.assistantName, file: path, metadata: options.metadata};
-
-    return api.uploadFile(request);
-  };
-}
+    // console.log("Type of path = ", typeof path);
+    //
+    // const request: UploadFileRequest = {assistantName: options.assistantName, file: path, metadata: options.metadata};
+    //
+    // return api.uploadFile(request);
+  // };
+};
