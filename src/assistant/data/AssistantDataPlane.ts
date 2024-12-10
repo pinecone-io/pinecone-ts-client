a109cbbf9d28b23d35bb1c56c2cf3339bb303c58
aulorbe <aulorbe@gmail.com>
2024-12-09 16:33:29 -0800

    Got list to work

--- src/assistant/data/AssistantDataPlane.ts
+++ src/assistant/data/AssistantDataPlane.ts
@@ -1,22 +1,32 @@
 import {
+  ListFilesRequest,
   ManageAssistantsApi as ManageAssistantsApiData, UploadFileRequest
 } from '../../pinecone-generated-ts-fetch/assistant_data';
 import { chatClosed, ChatRequest } from './chat';
 import { PineconeNotImplementedError } from '../../errors';
 import { uploadFileClosed, UploadRequest } from './uploadFile';
+import { ListFiles, listFilesClosed } from './listFiles';
 
 export class AssistantDataPlane {
   api: ManageAssistantsApiData;  // todo: should this be private?
   assistantName: string;
 
-  // private _chat: ReturnType<typeof chatClosed>;
-  private _uploadFile: ReturnType<typeof uploadFileClosed>;
   private _chat: ReturnType<typeof chatClosed>;
+  private _listFiles: ReturnType<typeof listFilesClosed>;
+
+  private _uploadFile: ReturnType<typeof uploadFileClosed>;
 
   constructor(api: ManageAssistantsApiData, assistantName: string) {
+    if (!assistantName) {
+      throw new Error('No assistant name provided');
+    }
+
     this.api = api;
     this.assistantName = assistantName;
     this._chat = chatClosed(this.assistantName, api);
+    this._listFiles = listFilesClosed(this.assistantName, api);
+
+
     this._uploadFile = uploadFileClosed(api);
   }
 
@@ -30,8 +40,11 @@ export class AssistantDataPlane {
   }
 
   // --------- File methods ---------
-  listFiles() {
-    return PineconeNotImplementedError;
+  listFiles(options?: ListFiles) {
+    if (!options) {
+      options = {};
+    }
+    return this._listFiles(options);
   }
 
   describeFile(){
---
+++ src/assistant/data/listFiles.ts
@@ -0,0 +1,15 @@
+import {
+  ListFilesRequest,
+  ManageAssistantsApi as ManageAssistantsApiData
+} from '../../pinecone-generated-ts-fetch/assistant_data';
+
+export interface ListFiles {
+  filter?: object;
+}
+
+export const listFilesClosed = (assistantName: string, api: ManageAssistantsApiData) => {
+  return async (options: ListFiles) => {
+    const request = {assistantName: assistantName, filter: options.filter} as ListFilesRequest;
+    return api.listFiles(request);
+  }
+}
