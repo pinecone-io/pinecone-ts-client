d0a7244d495f2525dc7c162058cd045c097ea8bc
aulorbe <aulorbe@gmail.com>
2024-12-10 15:42:12 -0800

    Keeping uploadFile as blob for user + got deleteFile working

--- src/assistant/data/uploadFile.ts
+++ src/assistant/data/uploadFile.ts
@@ -2,30 +2,15 @@ import {
   type AssistantFileModel,
   ManageAssistantsApi as ManageAssistantsApiData, UploadFileRequest
 } from '../../pinecone-generated-ts-fetch/assistant_data';
-import { promises as fs } from 'fs';
 
-
-export interface UploadRequest {
-  assistantName: string;
-  filePath: string;
+export interface UploadFile {
+  path: string;
   metadata?: string;
 }
 
-
-// async function filePathToBlob(filePath: string): Promise<Blob> {
-//   // Read the file as a Buffer
-//   const fileBuffer = await fs.readFile(filePath);
-//   // Create a Blob from the Buffer
-// }
-
-export const uploadFileClosed = (api: ManageAssistantsApiData) => {
-  // return async (options: UploadRequest): Promise<AssistantFileModel> => {
-    // const path = await filePathToBlob(options.filePath);
-
-    // console.log("Type of path = ", typeof path);
-    //
-    // const request: UploadFileRequest = {assistantName: options.assistantName, file: path, metadata: options.metadata};
-    //
-    // return api.uploadFile(request);
-  // };
+export const uploadFileClosed = (assistantName: string, api: ManageAssistantsApiData) => {
+  return async (options: UploadFileRequest): Promise<AssistantFileModel> => {
+    const request: UploadFileRequest = {assistantName: assistantName, file: options.file, metadata: options.metadata};
+    return await api.uploadFile(request);
+  };
 };
