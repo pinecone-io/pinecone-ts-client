9e2d340f2d55dfd00f6a28e9a09e2acd20fa652b
aulorbe <aulorbe@gmail.com>
2024-12-13 14:57:21 -0800

    Got uploadFile with metadata to work!

--- src/assistant/data/uploadFile.ts
+++ src/assistant/data/uploadFile.ts
@@ -10,7 +10,7 @@ import { buildUserAgent } from '../../utils';
 
 export interface UploadFile {
   path: string;
-  metadata?: { key: string; value: string } | { key: string; value: string }[];
+  metadata?: Record<string, string>;
 }
 
 export const uploadFileClosed = (
@@ -26,31 +26,24 @@ export const uploadFileClosed = (
     const form = new FormData();
     form.append('file', fs.createReadStream(filePath));
 
-    if (options.metadata) {
-      if (Array.isArray(options.metadata)) {
-        // If metadata is an array of key-value pairs
-        options.metadata.forEach(({ key, value }) => {
-          form.append(key, value);
-        });
-      } else {
-        // If metadata is a single key-value pair
-        const { key, value } = options.metadata;
-        form.append(key, value);
-      }
-    }
+    const reqHeaders = {
+      headers: {
+        ...form.getHeaders(),
+        ...(config.additionalHeaders || null),
+        'Api-Key': config.apiKey,
+        'User-Agent': buildUserAgent(config),
+        'X-Pinecone-Api-Version': X_PINECONE_API_VERSION,
+      },
+    };
 
-    return axios.post(
-      `https://prod-1-data.ke.pinecone.io/assistant/files/${assistantName}`,
-      form,
-      {
-        headers: {
-          ...form.getHeaders(),
-          ...(config.additionalHeaders || null),
-          'Api-Key': config.apiKey,
-          'User-Agent': buildUserAgent(config),
-          'X-Pinecone-Api-Version': X_PINECONE_API_VERSION,
-        },
-      }
-    );
+    let url = `https://prod-1-data.ke.pinecone.io/assistant/files/${assistantName}`;
+
+    if (options.metadata) {
+      const encodedMetadata = encodeURIComponent(
+        JSON.stringify(options.metadata)
+      );
+      url += `?metadata=${encodedMetadata}`;
+    }
+    return axios.post(url, form, reqHeaders);
   };
 };
