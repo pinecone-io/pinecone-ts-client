24b1261dd631d3cb7ef58611111aa4968b2b3630
aulorbe <aulorbe@gmail.com>
2024-12-16 10:41:56 -0800

    Add chatCompletions and contextAssistant

---
+++ src/assistant/data/context.ts
@@ -0,0 +1,28 @@
+import {
+  ContextAssistantRequest,
+  ManageAssistantsApi as ManageAssistantsApiData
+} from '../../pinecone-generated-ts-fetch/assistant_data';
+
+export interface Context {
+  query: string;
+  filter?: Record<string, string>;
+}
+
+export const contextClosed = (
+  assistantName: string,
+  api: ManageAssistantsApiData
+) => {
+  return async (options: Context) => {
+    if (!options.query) {
+      throw new Error('Must provide a query');
+    }
+    const request = {
+      assistantName: assistantName,
+      contextRequest: {
+        query: options.query,
+        filter: options.filter
+      }
+    } as ContextAssistantRequest;
+    return api.contextAssistant(request);
+  };
+};
