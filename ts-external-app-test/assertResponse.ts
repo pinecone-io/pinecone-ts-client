f3c6e59908a0f2eded26a0a9d23700f0968585f5
Austin DeNoble <austin.d@pinecone.io>
2025-05-05 19:51:37 -0400

    add a bit of sleep buffer after creating the index

--- ts-external-app-test/assertResponse.ts
+++ ts-external-app-test/assertResponse.ts
@@ -94,6 +94,9 @@ async function run() {
     console.log(`Creating test index: ${INDEX_NAME}`);
     await createIndex(apiKey);
 
+    // wait a bit for index creation before hitting describeIndexDetails
+    await new Promise((resolve) => setTimeout(resolve, 5000));
+
     console.log('Describing test index');
     const description = await describeIndexDetails(apiKey);
     console.log('Index description:', description);
